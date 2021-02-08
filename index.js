08f2175e64000d090ce0b48a0f6a31cb3095d8af
ashok <ashokreddy1613@gmail.com>
2021-02-08 17:15:00 -0500

    Deploy error fixing

--- index.js
+++ index.js
@@ -15,11 +15,24 @@ connectDB()
 
 const app = express();
 
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://gallery-pp.herokuapp.com/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 //body parser middleware
 // Takes the raw requests and turns them into usable properties on req.body
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
-app.use(cors());
+app.use(cors(corsOptions));
 
 //Cors middleware to allow the requests from different origins
 app.use((req, res, next) => {
@@ -46,11 +59,11 @@ app.use('/', userRoutes);
 
 const PORT = process.env.PORT || 5000
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get("*", (req, res)=> {
-    res.sendFile(path.join(__dirname, 'client','build', 'index.html')); //relative path
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html')); //relative path
   });
 
 }
