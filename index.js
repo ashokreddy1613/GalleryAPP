d28048fcfc1551d515c3064a83e33aa610f072dc
ashok <ashokreddy1613@gmail.com>
2021-02-08 15:20:27 -0500

    Preparing for deploy

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use('/', userRoutes);
 const PORT = process.env.PORT || 5000
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static('signupfrontend/build'));
+  app.use(express.static('client/build'));
 
   app.get("*", (req, res)=> {
     res.sendFile(path.join(__dirname, 'client','build', 'index.html')); //relative path
